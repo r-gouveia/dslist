cb153bab66abfe2667196107a7c46417a95e610e
Romulo Guilherme <guilherme.gouveiaa@hotmail.com>
2024-11-13 14:07:20 -0300

    repository,service,controller e dto para manipulaçao da saidade de infos na requisiçao

---
+++ src/main/java/com/example/dslist/service/GameService.java
@@ -0,0 +1,23 @@
+package com.example.dslist.service;
+
+import com.example.dslist.dto.GameMinDTO;
+import com.example.dslist.entities.Game;
+import com.example.dslist.repository.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GameService {
+
+    @Autowired
+    GameRepository gameRepository;
+
+    public List<GameMinDTO> findAll(){
+        List<Game> result = gameRepository.findAll();
+        List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+        return dto;
+    }
+
+}
